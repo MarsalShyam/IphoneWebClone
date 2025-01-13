6ca62e5fa83448ad9d6e2d036c47cbf31eb0b943
MarsalShyam <shyam12marsal@gmail.com>
2025-01-13 23:23:01 +0530

    work on model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,28 +1,93 @@
 
-import {useGSAP} from '@gsap/react';
+import { useGSAP } from '@gsap/react';
 import ModelView from './ModelView';
 import gsap from 'gsap';
-const Model = () => {
-    useGSAP(()=>{
-            gsap.to("#heading",{
-                opacity:1,
-                y:0,
-            })
-        },[]);
-  return (
-    <section className='common-padding'>
-        <div className='common-padding'>
-            <h1 id="heading" className='section-heading'>Take a closer look.
-            </h1>
+import { yellowImg } from "../utils"
+import * as THREE from 'three';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
 
-            <div className='flex flex-col items-center mt-5'>
-                <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
-                    <ModelView/>
+
+const Model = () => {
+
+    const [size, setSize] = useState('small');
+    const [model, setMethod] = useState({
+        title: 'iphone 15 pro in Natural Titanium',
+        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg,
+
+    })
+
+    //camera control for the model view
+    const cameraControlSmall = useRef();
+    const cameraControlLarge = useRef();
+
+    //model
+    const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
+
+    //rotatio
+    const [smallRotation, setSmallRotation] = useState(0);
+    const [largeRotation, setLargeRotation] = useState(0);
+
+    useGSAP(() => {
+        gsap.to("#heading", {
+            opacity: 1,
+            y: 0,
+        })
+    }, []);
+
+    return (
+        <section className='common-padding'>
+            <div className='common-padding'>
+                <h1 id="heading" className='section-heading'>Take a closer look.
+                </h1>
+
+                <div className='flex flex-col items-center mt-5'>
+                    <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
+                        <ModelView
+                            index={1}
+                            groupRef={small}
+                            gsapType="view1"
+                            controlRef={cameraControlSmall}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
+                        />
+                        <ModelView
+                            index={2}
+                            groupRef={large}
+                            gsapType="view2"
+                            controlRef={cameraControlLarge}
+                            setRotationState={setLargeRotation}
+                            item={model}
+                            size={size}
+                        />
+
+                        <Canvas
+                        className='w-full h-full'
+                        style={{
+                            position:'fixed',
+                            top:0,
+                            left:0,
+                            right:0,
+                            overflow:'hidden',
+                        }}
+                        eventSource={document.getElementById('root')}
+                        >
+                            <View.Port/>
+                        </Canvas>
+                    </div>
+                    <div className='mx-auto w-full'>
+                        <p className="">
+                            {model.title}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Model
